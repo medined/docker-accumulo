48adecb05d7f659b6c4da22b86668761069ce38f
David <david.medinets@gmail.com>
2014-07-10 19:36:21 -0400

    Add comment why accumulo supervisor configuration is done last.

--- single_node/init_accumulo.sh
+++ single_node/init_accumulo.sh
@@ -3,4 +3,6 @@ su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -mkdir -p /user/accumulo'
 su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /accumulo'
 su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /user/accumulo'
 su - accumulo -c '/usr/lib/accumulo/bin/accumulo init --instance-name accumulo --password secret'
+#
+# Now that accumulo has been initialized, we can add the supervisor configuration file.
 cp -vu /docker/supervisord-accumulo.conf /etc/supervisor/conf.d/accumulo.conf
