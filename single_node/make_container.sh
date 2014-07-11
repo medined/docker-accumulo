fc8e4f0a706af28c613605372b0a4722e88ff840
David <david.medinets@gmail.com>
2014-07-11 15:51:41 -0400

    Add bridge to Accumulo container

--- single_node/make_container.sh
+++ single_node/make_container.sh
@@ -2,9 +2,11 @@
 
 HOSTNAME=$1
 IMAGENAME=$2
+BRIDGENAME=$3
+SUBNET=$4
 
 usage() {
-  echo "Usage: $0 [host name] [image name]"
+  echo "Usage: $0 [host name] [image name] [bridge name] [class c subnet]"
   exit 1
 }
 
@@ -20,4 +22,26 @@ then
   usage
 fi
 
-sudo DOCKER_HOST=$DOCKER_HOST docker run -d --name=$IMAGENAME -h=$HOSTNAME -P medined/accumulo /docker/run.sh $HOSTNAME
+if [ -z $BRIDGENAME ]
+then
+  echo "Error: missing bridge name parameter."
+  usage
+fi
+
+if [ -z $SUBNET ]
+then
+  echo "Error: missing Class C subnet parameter. For example 10.0.10"
+  usage
+fi
+
+IPADDR="${SUBNET}.1"
+CIDR=24
+
+sudo DOCKER_HOST=$DOCKER_HOST docker run -d --name=$IMAGENAME -h=$HOSTNAME -P medined/accumulo /docker/run.sh $IMAGENAME $HOSTNAME $BRIDGENAME $IPADDR $CIDR
+
+echo "MAKE; BRIDGENAME: $BRIDGENAME"
+echo "MAKE; IMAGENAME: $IMAGENAME"
+echo "MAKE; $IPADDR/$CIDR"
+
+sudo DOCKER_HOST=$DOCKER_HOST ./pipework $BRIDGENAME $IMAGENAME "$IPADDR/$CIDR"
+sudo ifconfig $BRIDGENAME "${SUBNET}.254"
--- single_node/run.sh
+++ single_node/run.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 
-HOSTNAME=$1
+IMAGENAME=$1
+HOSTNAME=$2
+BRIDGENAME=$3
+IPADDR=$4
+CIDR=$5
+
+echo "--- RUN.sh -----"
+echo "|  IMAGENAME: $IMAGENAME"
+echo "|   HOSTNAME: $HOSTNAME"
+echo "| BRIDGENAME: $BRIDGENAME"
+echo "|     IPADDR: $IPADDR"
+echo "|       CIDR: $CIDR"
+echo "----------------"
 
 usage() {
-  echo "Usage: $0 [host name]"
+  echo "Usage: $0 [image name] [host name] [bridge name] [ipaddr] [cidr]"
   exit 1
 }
+
+if [ -z $IMAGENAME ]
+then
+  echo "Error: missing image name parameter."
+  usage
+fi
 
 if [ -z $HOSTNAME ]
 then
   echo "Error: missing host name parameter."
+  usage
+fi
+
+if [ -z $BRIDGENAME ]
+then
+  echo "Error: missing bridge name parameter."
+  usage
+fi
+
+if [ -z $IPADDR ]
+then
+  echo "Error: missing IP Address parameter."
+  usage
+fi
+
+if [ -z $CIDR ]
+then
+  echo "Error: missing CIDR parameter. Typically 24."
   usage
 fi
 
@@ -31,5 +67,7 @@ echo $HOSTNAME > /etc/accumulo/conf/masters
 echo $HOSTNAME > /etc/accumulo/conf/monitor
 echo $HOSTNAME > /etc/accumulo/conf/slaves
 echo $HOSTNAME > /etc/accumulo/conf/tracers
+
+/docker/pipework --wait $BRIDGENAME $IMAGENAME "$IPADDR/$CIDR"
 
 /usr/bin/supervisord -n
