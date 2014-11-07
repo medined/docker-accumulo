001c3c3092ce937bf37348f4cf8e6932423c8efc
David <david.medinets@gmail.com>
2014-11-06 22:29:21 -0500

    simplify script

--- single_node/init_accumulo.sh
+++ single_node/init_accumulo.sh
@@ -1,8 +1,8 @@
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -mkdir /accumulo'
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -mkdir -p /user/accumulo'
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /accumulo'
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /user/accumulo'
-su - accumulo -c '/usr/lib/accumulo/bin/accumulo init --instance-name accumulo --password secret'
+su - hdfs -c 'hdfs dfs -mkdir /accumulo'
+su - hdfs -c 'hdfs dfs -mkdir -p /user/accumulo'
+su - hdfs -c 'hdfs dfs -chown accumulo:accumulo /accumulo'
+su - hdfs -c 'hdfs dfs -chown accumulo:accumulo /user/accumulo'
+su - accumulo -c 'accumulo init --instance-name accumulo --password secret'
 #
 # Now that accumulo has been initialized, we can add the supervisor configuration file.
 cp -vu /docker/supervisord-accumulo.conf /etc/supervisor/conf.d/accumulo.conf
